96f8d8738bd9213445f4eaa7fb60e92b51d50cba
Emilien-L <emilienlecompte@gmail.com>
2026-03-24 17:18:13 +0100

    Fix rename playlist

--- frontend/src/app/services/playlist.service.ts
+++ frontend/src/app/services/playlist.service.ts
@@ -14,7 +14,7 @@ export interface PaginatedPlaylists {
 export class PlaylistService {
   private apiUrl = 'http://localhost:3000/api/playlists';
 
-  constructor(private http: HttpClient, private authService: AuthService) {}
+  constructor(private http: HttpClient) {}
 
   getAll(
     search: string = '',
