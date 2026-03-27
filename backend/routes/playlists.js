7f2c2a00dd761e4beff1a7ddf61a785565deb5e1
Emilien-L <emilienlecompte@gmail.com>
2026-03-27 12:48:45 +0100

    Supression de commentaires dans la route

--- backend/routes/playlists.js
+++ backend/routes/playlists.js
@@ -10,18 +10,8 @@ router.get('/my', authMiddleware, playlistController.getMyPlaylists);
 router.get('/liked', authMiddleware, playlistController.getLikedPlaylists);
 router.get('/:id', playlistController.getPlaylistById);
 
-router.post('/', authMiddleware, (req, res, next) => {
-    upload.single('cover')(req, res, (err) => {
-        if (err) {
-            // Ici on va enfin voir la VRAIE erreur dans la console !
-            console.error('🚨 Détail de l\'erreur d\'upload :', err);
-            return res.status(400).json({
-                message: 'Erreur lors de l\'upload de l\'image : ' + (err.message || 'Fichier non valide')
-            });
-        }
-        next();
-    });
-}, playlistController.createPlaylist);router.patch('/:id/cover', authMiddleware, upload.single('cover'), playlistController.updateCover);
+router.post('/', authMiddleware, upload.single('cover'), playlistController.createPlaylist);
+router.patch('/:id/cover', authMiddleware, upload.single('cover'), playlistController.updateCover);
 router.patch('/:id/rename', authMiddleware, playlistController.renamePlaylist);
 router.patch('/:id/songs', authMiddleware, playlistController.addSongs);
 router.post('/:id/like', authMiddleware, playlistController.toggleLike);
